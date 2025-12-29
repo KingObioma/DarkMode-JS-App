e3b109483486ab977f0b5db6ca4d356dac22b53c
king <kelvinozak0@gmail.com>
2025-12-29 10:32:46 +0100

    Little changes

--- final/data.js
+++ final/data.js
@@ -22,7 +22,7 @@ const articles = [
   },
   {
     id: 4,
-    title: 'what are the react king principles',
+    title: 'what are the react team principles',
     date: new Date(2015, 5, 4),
     length: 5,
     snippet: `Selvage street art hammock affogato VHS. Mustache shaman literally wayfarers schlitz. Direct trade four loko narwhal VHS pop-up, chartreuse trust fund typewriter street art swag thundercats art party.`,
