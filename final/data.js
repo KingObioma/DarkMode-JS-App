e9a97d8c05d52216cdcb354c35b75745c6255c0f
king <kelvinozak0@gmail.com>
2025-12-31 03:28:59 +0100

    few changes

--- final/data.js
+++ final/data.js
@@ -12,7 +12,6 @@ const articles = [
     date: new Date(2019, 10, 22),
     length: 5,
     snippet: `Biodiesel artisan seitan plaid sriracha copper mug venmo shabby chic. Kickstarter raclette kombucha, yr post-ironic jianbing try-hard flexitarian vaporware normcore.`,
-    snippet: `Biodiesel artisan seitan plaid sriracha copper mug venmo shabby chic. Kickstarter raclette kombucha, yr post-ironic jianbing try-hard flexitarian vaporware normcore.`,
   },
   {
     id: 3,
