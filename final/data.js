a43af76332654bc3de9e516f9e5076ca08a72100
king <kelvinozak0@gmail.com>
2026-01-05 23:17:36 +0100

    change config files for the update process

--- final/data.js
+++ final/data.js
@@ -28,7 +28,7 @@ const articles = [
     snippet: `Selvage street art hammock affogato VHS. Mustache shaman literally wayfarers schlitz. Direct trade four loko narwhal VHS pop-up, chartreuse trust fund typewriter street art swag thundercats art party.`,
   },
     {
-    id: 5,
+    id: 3,
     title: 'my decade in review',
     date: new Date(2018, 7, 11),
     length: 5,
