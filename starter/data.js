59c50a490385b3c8100b57507523855531363d13
king <kelvinozak0@gmail.com>
2026-01-10 22:38:01 +0100

    initialize DarkMode core architecture

--- starter/data.js
+++ starter/data.js
@@ -26,12 +26,5 @@ const articles = [
     date: new Date(2015, 5, 4),
     length: 5,
     snippet: `Selvage street art hammock affogato VHS. Mustache shaman literally wayfarers schlitz. Direct trade four loko narwhal VHS pop-up, chartreuse trust fund typewriter street art swag thundercats art party.`,
-  },
-   {
-    id: 5,
-    title: 'what are the react team principles',
-    date: new Date(2015, 5, 4),
-    length: 5,
-    snippet: `Selvage street art hammock affogato VHS. Mustache shaman literally wayfarers schlitz. Direct trade four loko narwhal VHS pop-up, chartreuse trust fund typewriter street art swag thundercats art party.`,
-  },
+  }
 ];
